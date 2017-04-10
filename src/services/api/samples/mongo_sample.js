335a65ff6d8928f98233649208b730a76a74cd38
Nathan <dobobobado@gmail.com>
2017-04-09 22:13:15 -0400

    updated store() method to storeOne()

--- src/services/api/samples/mongo_sample.js
+++ src/services/api/samples/mongo_sample.js
@@ -1,4 +1,4 @@
-import { sampleWildcard, addLikes, addDislikes, store } from '../mongodb'
+import { sampleWildcard, addLikes, addDislikes, storeOne } from '../mongodb'
 import config from '../config' // keys for twitter API
 
 const mongodb = require('mongodb')
@@ -78,7 +78,7 @@ function likesDislikesSample() {
         likes: 1,
         dislikes: 3,
       }
-      store(db, 'test', content)
+      storeOne(db, 'test', content)
       db.close()
     }
   })
