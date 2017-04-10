fbc0e85410b925b58d267ecb1c9db3945018f16a
Nathan <dobobobado@gmail.com>
2017-04-09 22:10:02 -0400

    added samples/ folder

---
+++ src/services/api/samples/twitter_sample.js
@@ -0,0 +1,18 @@
+import { getWildcardFromTwitter } from '../twitter'
+
+function twitterWildcardSample() {
+  try {
+    const daysBack = 5
+    getWildcardFromTwitter('movies', 1, daysBack, (result) => {
+      if (result) {
+        console.log('FOUND TWEET: \n', result)
+      } else {
+        console.error('ERROR: unable to get tweet.')
+      }
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+twitterWildcardSample()
